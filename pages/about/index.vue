d8caa9d448700a475c477be52c34fdd5845f7b93
Breno Silva <brenosilva1@gmail.com>
2018-03-26 23:14:44 -0300

    about page storyblok

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,6 +1,36 @@
 <template>
   <section id="about-page">
-      <h1>Hi, that us</h1>
-      <p>We're bunch o highly motivated people who like blogs</p>
+      <h1>{{ title }}</h1>
+      <p>{{ content }}</p>
   </section>
 </template>
+
+
+<script>
+export default {
+  asyncData(context){
+    return context.app.$storyapi.get('cdn/stories/about', {
+      version: 'draft'
+    }).then(res => {
+      console.log(res.data);
+      return {
+        title: res.data.story.content.title,
+        content: res.data.story.content.content
+      }
+    })
+  }
+}
+</script>
+
+
+<style scoped>
+#about-page{
+  width: 80%;
+  max-width: 500px;
+  margin: auto;
+}
+
+#about-page p {
+  white-space: pre-line;
+}
+</style>
